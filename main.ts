69c461f94982297b2e8ae455ea7de4b1f9289159
aleksazskola <93650537+aleksazskola@users.noreply.github.com>
2021-11-05 15:38:41 +0100

    Ажурирање main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "AAB") {
+        radio.sendString("mystery")
+    } else if (receivedString == "ABB") {
+        radio.sendString("paris")
+    }
+})
+radio.setGroup(101)
 basic.forever(function () {
 	
 })
