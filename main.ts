a933aad7b37e2a97bc165073f2caaec0f6e32bc4
aleksazskola <93650537+aleksazskola@users.noreply.github.com>
2021-11-05 15:39:55 +0100

    Ажурирање pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "AAB") {
         radio.sendString("mystery")
+        basic.showIcon(IconNames.Yes)
     } else if (receivedString == "ABB") {
         radio.sendString("paris")
+        basic.showIcon(IconNames.Yes)
     }
 })
 radio.setGroup(101)
